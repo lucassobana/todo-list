1f7a52162baad7e514f0ea663f9211771f073818
LucasObana <lucas.sawada@ges.inatel.br>
2025-05-01 18:50:34 -0300

    Deleted useless comments

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,7 @@ import { Task } from "./components/Task";
 import './global.css'
 import styles from './App.module.css'
 
-// export interface Task {
-//     id: number
-//     text: string
-//     isChecked: boolean
-// }
-
 export function App() {
-    // const [tasks, setTasks] = useState<Task[]>([])
-    // const [newTask, setNewTask] = useState('');
-
     return (
         <main>
             <Header />
